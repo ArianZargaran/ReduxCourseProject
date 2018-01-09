48c1649e36f520a962c79479e74eed56a240734c
Arian Zargaran <arianzf@gmail.com>
2018-01-09 00:26:25 -0800

    Container created

---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The good parts' },
+    { title: 'Grokking Algorithms' },
+    { title: 'JS for dummies' },
+    { title: 'ReactJS the V of MVC' },    
+  ]
+}
